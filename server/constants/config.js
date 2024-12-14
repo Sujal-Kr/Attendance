84ce91d99f615b82bd76afe5bb8707e334c3b4dd
Sujal-Kr <sunnykumarcms45@gmail.com>
2024-12-14 23:34:20 +0530

    minor changes

--- server/constants/config.js
+++ server/constants/config.js
@@ -1,4 +1,4 @@
 export const corsOption = {
-    origin: ["http://localhost:5173", "http://localhost:4173"],
+    origin: ["http://localhost:5173", "http://localhost:4173",process.env.CLIENT_URL],
     credentials: true  
 };
